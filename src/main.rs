30af9a64ddd824f691cea7b36b3844eeed7a1246
naonao708 <naonao708@outlook.com>
2022-12-31 05:13:20 +0000

    Change impl bool

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,16 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
 
-    println!("rect1 is {:?}", rect1);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
